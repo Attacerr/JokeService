c97a593cb66cc40e0e9a3711c9190b00a48feeca
Marcus Berg Andersen <MarcusBerg@live.dk>
2020-11-08 21:58:37 +0100

    fix

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const config = require('./config');
+const fetch = require('node-fetch')
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
